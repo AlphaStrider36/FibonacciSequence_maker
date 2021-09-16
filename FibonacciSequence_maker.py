52a0c91d8a06edb070d93f63bab5e7ba647f2548
AlphaStrider36 <79163784+AlphaStrider36@users.noreply.github.com>
2021-09-16 20:32:02 +0400

    Add files via upload

---
+++ FibonacciSequence_maker.py
@@ -0,0 +1,9 @@
+def fibonacci_of(n):
+    if n in {0, 1}:
+        return n
+    return fibonacci_of(n - 1) + fibonacci_of(n - 2)
+
+
+for n in range(15):
+    output = fibonacci_of(n)
+    print(output)
